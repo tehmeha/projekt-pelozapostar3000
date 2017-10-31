0cd9d7513fd9b6d563e83dc6bdf8f886b35bf9d8
pelozapostar3000 <jakov.cretnik@gmail.com>
2017-10-31 15:35:36 +0100

    novo

--- program.cpp
+++ program.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 int main ()
 {
+    int oznaka_susreta [10] = (7119, 7117, 7123, 7122, 7125, 7126, 7124, 7118,1186, 7143);
+
+    string naziv [10] = ("Mancester utd.-Benfica", "Atletico Madrid-Qarabakh", "Celtic-Bayern", "PSG-Anderlecht", "Sporting L.-Juventus", "Olympiakos-Barcelona", "Roma-Chelsea"    )
+
     return 0;
 }
